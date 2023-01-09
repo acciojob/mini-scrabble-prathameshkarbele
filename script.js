3277f2fa17daf3554b0677101a4a8938fc4d9b8e
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-01-09 15:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+ const inputField = document.getElementById("evaluatedText");
+  const letterCountElement = document.getElementById("letterCount");
+
+  // Set an event listener on the input field to update the letter count when the text changes
+  inputField.addEventListener("input", function() {
+    // Update the letter count
+    letterCountElement.textContent = inputField.value.length;
+  });
